4501be159ddd7a35dd353b97fc40bdfa5b3af158
NBreunesse <129849615+NBreunesse@users.noreply.github.com>
2023-04-12 15:52:06 +0200

    Add files via upload

---
+++ psych_ampTry4.js
@@ -0,0 +1,58 @@
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/NBreunesse/AMP-example@main/qampTry4.js'], function(APIConstructor, ampExtension){
+
+	var API = new APIConstructor();
+	
+	
+	return ampExtension({
+		primeCats :  [
+			{
+				nameForFeedback : 'Psychedelic',  //Will be used in the user feedback 
+				nameForLogging : 'Psychedelic', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'Psychedelics'}, 
+					{word : 'Hallucinogen'}, 
+					{word : 'LSD'}, 
+					{word : 'Psilocybin'}, 
+					{word : 'Mescaline'}, 
+					{word : 'DMT'}, 
+					{word : 'Ayahuasca'}, 
+					{word : 'Ecstasy'}, 
+					{word : 'Ketamine'}, 
+					{word : 'Peyote'}, 
+					{word : 'Psychonaut'}, 
+					{word : 'Tripping'}]
+
+			}, 
+			{
+				nameForFeedback : 'Control',  //Will be used in the user feedback 
+				nameForLogging : 'Control', //Will be used in the logging
+				//An array of all media objects for this category.
+				mediaArray : [
+				    {word : 'Antidepressant'}, 
+					{word : 'Ritalin'}, 
+					{word : 'Aspirin'}, 
+					{word : 'Ibuprofen'}, 
+					{word : 'Adderall'}, 
+					{word : 'Oxycodon'}, 
+					{word : 'Prozac'}, 
+					{word : 'Lexapro'}, 
+					{word : 'Wellbutrin'}, 
+					{word : 'Zoloft'}, 
+					{word : 'SSRI'}, 
+					{word : 'Fluoxetine'}]
+			}
+		],
+
+		examplePrimeStimulus : 
+		{
+			nameForLogging : 'examplePrime', //Will be used in the logging
+			//An array of all media objects for this category.
+			mediaArray : [{word : 'Umbrella'}, {word : 'Table'}, {word : 'Hammer'}]
+		},
+
+		base_url : {//Where are your images at?
+			image : 'https://baranan.github.io/minno-tasks/images/ampImages'
+		}
+	});
+});
